9a5fd6d3b6e2c0c035c8b5f517689918f421fe86
findeway <findewayz@gmail.com>
2015-04-24 15:47:38 +0800

    extract floatingsatellitemenu from old project;

---
+++ FloatingSatelliteMenu-Samples/src/main/java/com/findeway/FloatingSatelliteMenu/Samples/SMMainActivity.java
@@ -0,0 +1,23 @@
+package com.findeway.floatingsatellitemenu.Samples;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.findeway.floatingsatellitemenu.SatelliteMenu;
+
+public class SMMainActivity extends Activity implements SatelliteMenu.OnMenuItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smmain);
+        SatelliteMenu menu = (SatelliteMenu) findViewById(R.id.satelliteMenu);
+        menu.setOnMenuItemClickListener(this);
+    }
+
+    @Override
+    public void OnMenuItemClick(int itemId) {
+        Log.i("item clicked", String.valueOf(itemId));
+    }
+}
